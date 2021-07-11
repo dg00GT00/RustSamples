d9da71634356b1ed818cedeb6252ae724a8785b7
Gledson Duarte <duarte.gledson@gmail.com>
2021-07-11 16:07:11 -0300

    Inserted a sample struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
-fn main() {
-    let s = String::from("hello world");
-    let hello = first_word(&s);
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 
-    println!("Testing: {}", hello);
-}
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[..i];
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
         }
     }
-    &s[..]
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let square = Rectangle::square(3);
+
+    println!("This is a square: {:?}", square);
+    
+    println!(
+        "The area of the rectangle is {} square pixels",
+        rect1.area()
+    );
+
+    println!("rect1 is {:#?}", rect1);
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
