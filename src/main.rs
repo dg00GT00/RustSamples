f5e5e4d43c323d6b0b75480c753514c52b267097
Gledson Duarte <duarte.gledson@gmail.com>
2021-09-25 08:46:08 -0300

    Used shortcut propagation operator

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+use std::error::Error;
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{ErrorKind, Read};
+use std::io;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
 
 fn main() {
-    match File::open("hello.txt") {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => panic!("Problem opening the file: {:?}", other_error)
-        }
+    match read_username_from_file() {
+        Err(e) => println!("{}", e),
+        Ok(s) => println!("{}", s)
     };
 }
