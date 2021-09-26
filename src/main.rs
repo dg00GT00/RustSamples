c72e80c89b3e3fd7b053a0e6138bcec5d5de5507
Gledson Duarte <duarte.gledson@gmail.com>
2021-09-26 09:48:35 -0300

    Mixed up generic types

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,22 @@
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
-//     for &item in list.iter() {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
-
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
 fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
-    // let result = largest(&number_list);
-    // println!("The largest number is {}", result);
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
 
-    let p = Point { x: 5, y: 10 };
-    println!("p.x = {}", p.x());
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
