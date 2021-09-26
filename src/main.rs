1b08b6a0efb8746a802f372f66486965547335b5
Gledson Duarte <duarte.gledson@gmail.com>
2021-09-26 08:26:08 -0300

    Implemented mod system from Rust by example online book

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
-use std::error::Error;
-use std::fs::File;
-use std::io::{ErrorKind, Read};
-use std::io;
+mod my;
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    let mut f = File::open("hello.txt")?;
-    let mut s = String::new();
-    f.read_to_string(&mut s)?;
-    Ok(s)
+fn function() {
+    println!("called `function()`");
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut f = File::open("hello.txt")?;
-
-    match read_username_from_file() {
-        Err(e) => println!("{}", e),
-        Ok(s) => println!("{}", s)
-    };
-
-    Ok(())
+fn main() {
+    my::function();
+    function();
+    my::indirect_access();
+    my::indirect_access_from_inaccessible();
+    my::nested::function();
 }
