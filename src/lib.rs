e12b43871b7f4e35e827a7cfe5aeebe1e9e6254f
Gledson Duarte <duarte.gledson@gmail.com>
2021-10-10 15:48:09 -0300

    Implemented a first simple test

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod test {
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-struct ImportantExcept<'a> {
-    par: &'a str,
-}
+use std::fmt::Display;
 
-impl<'a> ImportantExcept<'a> {
-    fn announce_and_return_par(&self, announcement: &str) -> &str {
-        println!("Attention please: {}", announcement);
-        self.par
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
 
