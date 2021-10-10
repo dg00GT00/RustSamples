091d1870e6c04975f93c1b20ebc0bf264988a76d
Gledson Duarte <duarte.gledson@gmail.com>
2021-10-10 09:39:47 -0300

    Fixed the largest function implementation

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,15 @@
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
-//     for &item in list.iter() {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
-
-struct Point<T> {
-    x: T,
-    y: T,
-}
-
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
     }
+    largest
 }
 
 fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
-    // let result = largest(&number_list);
-    // println!("The largest number is {}", result);
-
-    let p = Point { x: 5, y: 10 };
-    println!("p.x = {}", p.x());
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 }
