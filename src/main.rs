c6c2f0fe945d0117d58b1c3f49f1020abe22e9b0
Gledson Duarte <duarte.gledson@gmail.com>
2021-10-11 09:10:29 -0300

    Exited with an error code using closure if creating a new Config fails

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, process};
 use std::fs;
 
 struct Config {
@@ -7,16 +7,21 @@ struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Self {
+    pub fn new(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 { return Err("not enough parameters"); }
+
         let query = args[1].clone();
         let filename = args[2].clone();
-        Self { query, filename }
+        Ok(Self { query, filename })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        print!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}\n", config.filename);
