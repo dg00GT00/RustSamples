f38c6165a70396dc229ec8ebbae37ddc7402a4f1
Gledson Duarte <duarte.gledson@gmail.com>
2021-10-12 17:32:45 -0300

    Wrote error message to standard error instead of standard output using eprintln!

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod parser;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        print!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -15,7 +15,7 @@ fn main() {
     println!("In file {}\n", config.filename);
 
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
