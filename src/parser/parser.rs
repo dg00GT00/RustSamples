a6d19e94f1ab11d8f20100065bd990acf9c994e9
Gledson Duarte <duarte.gledson@gmail.com>
2021-10-12 07:54:36 -0300

    Inserted option for case-insensitive search

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,9 +1,10 @@
+use std::{env, fs};
 use std::error::Error;
-use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -12,14 +13,22 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Self { query, filename })
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Self { query, filename, case_sensitive })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -30,7 +39,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
     for line in contents.lines() {
-        if line.contains(query) { results.push(line) }
+        if line.contains(query) { results.push(line); }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
     }
 
     results
