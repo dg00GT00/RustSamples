5a7dd1da5720e3622beefce52c6f33da53a2caf2
Gledson Duarte <duarte.gledson@gmail.com>
2021-11-15 10:36:23 -0300

    Inserted multiple references with Rc

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-struct CustomSmartPointer {
-    data: String,
-}
+use std::rc::Rc;
 
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
-    }
+use crate::List::{Cons, Nil};
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
 fn main() {
-    let c = CustomSmartPointer { data: String::from("my stuff") };
-    let d = CustomSmartPointer { data: String::from("other stuff") };
-    println!("CustomSmartPointer created");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
