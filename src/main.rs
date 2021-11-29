b72e5b2e71cfa78a6f8093b5b43e02b181b6639c
Gledson Duarte <dg_gt@outlook.com>
2021-11-29 06:24:23 -0300

    Received the value "hi" in the main thread and printing it

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let v = vec![1, 2, 3];
-    let handle = thread::spawn(move || {
-        println!("Here's a vector: {:?}", v);
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
     });
-    handle.join().unwrap();
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 }
