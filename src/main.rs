9d3377525fd81d0d59e5f93578d1f7b946e4645e
Gledson Duarte <dg_gt@outlook.com>
2021-11-30 07:32:32 -0300

    Implemented an Arc<T> to wrap the Mutex<T> to be able to share ownership across multiple threads - This code do not compile

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,24 @@
-use std::sync::mpsc;
+use std::rc::Rc;
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    let tx1 = mpsc::Sender::clone(&tx);
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
+        for handle in handles {
+            handle.join().unwrap();
         }
-    });
-
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {}", received);
     }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
