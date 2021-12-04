5b49dd47fe137f902d6a106e2dfa0466b7ae2e0d
Gledson Duarte <dg_gt@outlook.com>
2021-12-04 12:30:51 -0300

    Implemented state pattern example

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,81 @@
-pub struct Button {
-    pub width: u32,
-    pub height: u32,
-    pub label: String,
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
 }
 
-pub trait Draw {
-    fn draw(&self);
-}
+impl Post {
+    pub fn new() -> Self {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
 
-impl Draw for Button {
-    fn draw(&self) {
-        todo!()
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(&self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
     }
 }
 
-pub struct Screen {
-    pub components: Vec<Box<dyn Draw>>,
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, _post: &'a Post) -> &'a str {
+        ""
+    }
 }
 
-impl Screen {
-    pub fn run(&self) {
-        for component in self.components.iter() {
-            component.draw();
-        }
+struct PendingReview;
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct Published;
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,13 @@
-use hello_cargo::{Button, Draw, Screen};
-
-struct SelectBox {
-    width: u32,
-    height: u32,
-    options: Vec<String>,
-}
-
-impl Draw for SelectBox {
-    fn draw(&self) {
-        todo!()
-    }
-}
+use hello_cargo::Post;
 
 fn main() {
-    let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No"),
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ]
-    };
+    let mut post = Post::new();
+    post.add_text("I ate salad for lunch today");
+    assert_eq!("", post.content());
 
-    screen.run();
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate salad for lunch today", post.content());
 }
