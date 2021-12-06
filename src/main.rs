a2702339ec9a2b69a714894ab64e317270aba8ba
Gledson Duarte <dg_gt@outlook.com>
2021-12-06 07:26:24 -0300

    Mixed if let, else, else if let and else

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-use hello_cargo::Post;
-
 fn main() {
-    let mut post = Post::new();
-    post.add_text("I ate salad for lunch today");
-    let post = post.request_review();
-    let post = post.approve();
-    assert_eq!("I ate salad for lunch today", post.content());
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {}, as the background", color);
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
 }
