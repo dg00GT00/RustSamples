7b3606f661f5a66a8d2dd4f2210b0c3484c91a3d
Gledson Duarte <dg_gt@outlook.com>
2021-12-06 06:32:59 -0300

    Modified the main to use the new implementation of the blog post workflow

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,7 @@ use hello_cargo::Post;
 fn main() {
     let mut post = Post::new();
     post.add_text("I ate salad for lunch today");
-    assert_eq!("", post.content());
-
-    post.request_review();
-    assert_eq!("", post.content());
-
-    post.approve();
+    let post = post.request_review();
+    let post = post.approve();
     assert_eq!("I ate salad for lunch today", post.content());
 }
