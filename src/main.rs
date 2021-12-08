a932b7d3e73f6328df8465a811ea5485acb8ad4f
Gledson Duarte <dg_gt@outlook.com>
2021-12-08 07:10:42 -0300

    Implemented destructing and matching literal values in one pattern

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-fn print_coordinates(&(x, y): &(i32, i32)){
-    println!("Current location: ({}, {})", x, y);
+struct Point {
+    x: u16,
+    y: u16
 }
 
 fn main() {
-    let x = Some(5);
-    let y = 10;
-
-    match x {
-        Some(50) => println!("Got 50"),
-        Some(y) => println!("Matched, y = {:?}", y),
-        _ => println!("Default case, x = {:?}", x)
+    let p = Point { x: 0, y: 7 };
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
     }
-
-    println!("at the end: x = {:?}, y = {:?}", x, y);
 }
