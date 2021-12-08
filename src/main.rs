d03c1516124976b437a3c775dd25fe85c6b8b1cb
Gledson Duarte <dg_gt@outlook.com>
2021-12-08 09:25:30 -0300

    A match expression with an arm that introduces a shadowed variable y

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+fn print_coordinates(&(x, y): &(i32, i32)){
+    println!("Current location: ({}, {})", x, y);
+}
+
 fn main() {
-    let v = vec!['a', 'b', 'c'];
-    for (index, value) in v.iter().enumerate() {
-        println!("{} is at index {}", value, index);
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x)
     }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
 }
