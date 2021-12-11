5cdda9f466ed78f97389541ec68e8b5b4b5e33b9
Gledson Duarte <duarte.gledson@gmail.com>
2021-12-11 15:24:46 -0300

    Read from or writing to a mutable static variable is unsafe

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-use std::slice;
+static mut COUNTER : u32 = 0;
 
-fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
-    let len = slice.len();
-    let ptr = slice.as_mut_ptr();
-
-    assert!(mid <= len);
-
-    unsafe {
-        (slice::from_raw_parts_mut(ptr, mid),
-         slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid))
-    }
+fn add_to_count(inc: u32) {
+    unsafe { COUNTER += inc; }
 }
 
 fn main() {
-
+    add_to_count(3);
+    unsafe { println!("Counter: {}", COUNTER); }
 }
