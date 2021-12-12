ebab6b7117ca153522ab7434029c5b2c36723cb0
Gledson Duarte <duarte.gledson@gmail.com>
2021-12-12 16:11:43 -0300

    Implemented the Add trait on Millimeters to add Millimeters to Meters

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
 use std::ops::Add;
 
-#[derive(Debug, PartialEq)]
-struct Point {
-    x: i32,
-    y: i32,
-}
+struct Millimeters(u32);
+struct Meters(u32);
 
-impl Add for Point {
-    type Output = Point;
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
 
-    fn add(self, other: Self) -> Self::Output {
-        Point {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
+    fn add(self, other: Meters) -> Self::Output {
+        Millimeters(self.0 + (other.0 * 1000))
     }
 }
 
 fn main() {
-    assert_eq!(Point { x: 1, y: 0 } + Point { x: 2, y: 3 }, Point { x: 3, y: 3 });
 }
