f00384975467e062a7667293c58aec131dd0b339
Gledson Duarte <duarte.gledson@gmail.com>
2021-12-12 17:05:37 -0300

    Used fully qualified syntax to specify that we want to call the baby_name function from the Animal trait as implemented on Dog

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,21 @@
-trait Pilot {
-    fn fly(&self);
+trait Animal {
+    fn baby_name() -> String;
 }
 
-trait Wizard {
-    fn fly(&self);
-}
+struct Dog;
 
-struct Human;
-
-impl Pilot for Human {
-    fn fly(&self) {
-        println!("This is your captain speaking.");
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
     }
 }
 
-impl Wizard for Human {
-    fn fly(&self) {
-        println!("Up!");
-    }
-}
-
-impl Human {
-    fn fly(&self) {
-        println!("*waving arms furiously*");
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
     }
 }
 
 fn main() {
-    let person = Human;
-    Pilot::fly(&person);
-    Wizard::fly(&person);
-    person.fly();
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
 }
