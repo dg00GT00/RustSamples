ce6c12786e4c982cd37dabe8ecd8c910fb6587cb
Gledson Duarte <duarte.gledson@gmail.com>
2021-12-12 16:03:28 -0300

    Implemented the Add trait to overload the + operator for Point instances

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-static mut COUNTER : u32 = 0;
+use std::ops::Add;
 
-fn add_to_count(inc: u32) {
-    unsafe { COUNTER += inc; }
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Self) -> Self::Output {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
 }
 
 fn main() {
-    add_to_count(3);
-    unsafe { println!("Counter: {}", COUNTER); }
+    assert_eq!(Point { x: 1, y: 0 } + Point { x: 2, y: 3 }, Point { x: 3, y: 3 });
 }
