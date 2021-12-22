4bdc10f1ec3a625b51bfc6b4f7d9175685da4af7
Gledson Duarte <duarte.gledson@gmail.com>
2021-12-22 07:02:43 -0300

    Responded with status code 404 and an error page if anything other than was requested

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
         handle_connection(stream);
     }
 }
@@ -16,9 +15,18 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let get = b"GET / HTTP/1.1\r\n";
 
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        stream.write_all(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!("{}{}", status_line, contents);
+        stream.write_all(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
