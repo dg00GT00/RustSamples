f51e0ddd5ab860a948d57baa75b0b8307bc8ac01
Gledson Duarte <duarte.gledson@gmail.com>
2021-12-25 09:38:30 -0300

    Shred the receiving end of the channel among the workers using Arc and Mutex

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use std::sync::{Arc, mpsc, Mutex};
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+        Worker { id, thread }
+    }
+}
+
+struct Job;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// # Arguments
+    ///
+    /// * `size`: It is the number of threads in the pool
+    ///
+    /// # Panics
+    /// The 'new' function will panic if the size is zero
+    ///
+    /// returns: ThreadPool
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)))
+        }
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static {}
+}
